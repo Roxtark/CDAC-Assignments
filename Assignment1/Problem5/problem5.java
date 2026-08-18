34e31bb96b4092fd61dbc6dec0ab2c0390b0247c
Rohit Katkar <163819078+Roxtark@users.noreply.github.com>
2026-08-18 18:03:10 +0530

    Add files via upload

---
+++ Assignment1/Problem5/problem5.java
@@ -0,0 +1,11 @@
+public class problem5 {
+    public static void main(String[] args) {
+
+        int side = 5;
+
+        int area = side * side;
+
+        System.out.println("Area = " + area);
+    }
+    
+}
