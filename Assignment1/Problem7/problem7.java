ec9ebe7cd08689ae545c37a6110ab08a513f06a2
Rohit Katkar <163819078+Roxtark@users.noreply.github.com>
2026-08-18 18:05:26 +0530

    Add files via upload

---
+++ Assignment1/Problem7/problem7.java
@@ -0,0 +1,24 @@
+public class problem7 {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 25;
+        int c = 15;
+
+        int largest;
+
+        if (a > b && a > c) {
+            largest = a;
+        } 
+        else if (b > a && b > c) {
+            largest = b;
+        } 
+        else {
+            largest = c;
+        }
+
+        System.out.println("Largest = " + largest);
+    }
+    
+}
