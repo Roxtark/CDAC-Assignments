2ba6931fb6de85efcd6edbea84c35d6d45073eb4
Rohit Katkar <163819078+Roxtark@users.noreply.github.com>
2026-08-18 18:02:25 +0530

    Add files via upload

---
+++ Assignment1/Problem4/problem4.java
@@ -0,0 +1,12 @@
+public class problem4 {
+
+    public static void main(String[] args) {
+
+        int time = 9;
+
+        if (time >= 5 && time <= 12) {
+            System.out.println("Good Morning");
+        }
+    }
+    
+}
