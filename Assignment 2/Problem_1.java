977b2db797f2ab38e899cf529d300d677f31b2b6
Rohit Katkar <163819078+Roxtark@users.noreply.github.com>
2026-08-23 11:14:06 +0530

    Add files via upload

---
+++ Assignment 2/Problem_1.java
@@ -0,0 +1,28 @@
+public class Problem_1 {
+
+    public static void main(String[] args) {
+
+        int maths = 80;
+        int science = 85;
+        int history = 90;
+
+        int average = (maths + science + history) / 3;
+
+        char grade;
+
+        if (average >= 90) {
+            grade = 'A';
+        } else if (average >= 70) {
+            grade = 'B';
+        } else if (average >= 50) {
+            grade = 'C';
+        } else if (average >= 30) {
+            grade = 'D';
+        } else {
+            grade = 'F';
+        }
+
+        System.out.println("Average Marks: " + average);
+        System.out.println("Grade: " + grade);
+    }
+}
---
+++ Assignment 2/Problem_4.java
@@ -0,0 +1,24 @@
+public class Problem_4 {
+
+    static byte a = 10;
+    static short b = 20;
+    static int c = 30;
+    static long d = 40L;
+    static float e = 50.5f;
+    static double f = 60.5;
+    static char g = 'A';
+    static boolean h = true;
+
+    public static void main(String[] args) {
+
+        System.out.println("byte: " + a);
+        System.out.println("short: " + b);
+        System.out.println("int: " + c);
+        System.out.println("long: " + d);
+        System.out.println("float: " + e);
+        System.out.println("double: " + f);
+        System.out.println("char: " + g);
+        System.out.println("boolean: " + h);
+    }
+    
+}
